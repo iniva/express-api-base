c35884c95f5adc84233482bc2567b07d08ddcfe3
Tony <tonyblast@gmail.com>
2018-08-06 07:19:12 -0300

    [dev-1.0] refactor: Use utils, configs and middlewares in main script

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import express from 'express';
 
+import Logger from 'Utils/logger';
+import Config from './config';
+// Global Middlewares
+import globalMiddlewares from 'Middlewares/global';
 // API Modules
 import health from 'Modules/health';
 
+const log = Logger.create();
 const app = express();
+const options = Config.get('server');
+
+// Init App settings
+const settings = Config.get('app');
+
+for (const [setting, value] of Object.entries(settings)) {
+    app.set(setting, value);
+}
+
+// Set Global Middlewares
+app.use('/*', globalMiddlewares);
 
 // Load API Modules
 app.use(health.prefix, health.routes);
 
 // Start App
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`${process.env.APP_NAME} listening on port http://0.0.0.0:${process.env.SERVER_PORT}`);
+app.listen(options.port, options.host, () => {
+    log(`${app.get('name')} ${app.get('version')} listening on port http://0.0.0.0:${options.port}`);
 });
