90f925606e5ab425a42b1eb50dc4b8bed9f08968
Tony <tonyblast@gmail.com>
2018-08-06 07:12:34 -0300

    [dev-1.0] chore: Added "Middlewares" alias to babel and webstorm config files

--- webstorm.js
+++ webstorm.js
@@ -2,6 +2,7 @@ System.config({
     'paths': {
         'Config/*': './src/config/*',
         'Modules/*': './src/modules/*',
+        'Middlewares/*': './src/middlewares/*',
         'Utils/*': './src/utils/*'
     }
 });
