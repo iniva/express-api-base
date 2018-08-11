7a19dc3892a189a0db5e423ed0ebdaa5a8d3547e
Tony <tonyblast@gmail.com>
2018-08-11 12:26:38 -0300

    [dev-1.0] refactor: Use wrapAsync middleware in Health Controller

--- src/modules/health/index.js
+++ src/modules/health/index.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import HealthController from './controller';
+import wrapAsync from 'Middlewares/wrapAsync';
 
 const PREFIX = '/health';
 const routes = Router(); // eslint-disable-line new-cap
 
 routes
-    .get('/', HealthController.get);
+    .get('/', wrapAsync(HealthController.get));
 
 export default {
     prefix: PREFIX,
