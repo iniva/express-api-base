6dd10b22dbb254e9f7e2115bc0c28f94efda46d3
Tony <tonyblast@gmail.com>
2018-08-11 12:30:25 -0300

    [dev-1.0] refactor: Throw error in HealthController get method to show wrapAsync flow

--- src/modules/health/controller.js
+++ src/modules/health/controller.js
@@ -1,5 +1,13 @@
 export default class HealthController {
     static async get(req, res) {
+        if (req.query.hasOwnProperty('fail')) {
+            const error = new Error('oh dear');
+
+            error.statusCode = 400;
+
+            throw error;
+        }
+
         res.send({
             data: {
                 message: 'All good here, thanks for asking!'
