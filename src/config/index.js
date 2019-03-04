04d8e80cf4cf1d301fb6edf14ea0ce0b20659ad2
Tony <tonyblast@gmail.com>
2019-03-04 08:10:54 -0300

    [v2-dev] refactor(config/index): Lint fixes + new constants + new pino opts

--- src/config/index.js
+++ src/config/index.js
@@ -1,80 +1,81 @@
+import os from 'os';
 import merge from 'webpack-merge';
 import Path from 'path';
 
 import * as packageInfo from '../../package';
 
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const DEFAULT_SERVER_PORT = 8091;
-const DEFAULT_CACHE_TTL = 3600;
+const SERVER_HOST = process.env.SERVER_HOST || '0.0.0.0';
+const SERVER_PORT = process.env.SERVER_PORT || 8091;
+const CACHE_DRIVER = process.env.CACHE_DRIVER || 'memory';
+const CACHE_DEFAULT_TTL = process.env.CACHE_DEFAULT_TTL || 3600;
 const API_VERSION = packageInfo.version;
-const APP_NAME = process.env.APP_NAME || packageInfo.name;
+const APP_NAME = process.env.APP_NAME || 'Express API';
 
 const rootDir = Path.dirname(require.main.filename || process.mainModule.filename);
 const defaultConfig = {
-    rootDir,
+  rootDir,
 
-    app: {
-        name: APP_NAME,
-        version: API_VERSION,
-        env: ENVIRONMENT,
-        'case sensitive routing': true,
-        'strict routing': false,
-        'x-powered-by': false
-    },
+  app: {
+    name: APP_NAME,
+    version: API_VERSION,
+    env: ENVIRONMENT,
+    'case sensitive routing': true,
+    'strict routing': false,
+    'x-powered-by': false,
+  },
 
-    server: {
-        host: process.env.SERVER_HOST || '0.0.0.0',
-        port: process.env.SERVER_PORT || DEFAULT_SERVER_PORT
-    },
+  server: {
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+  },
 
-    http: {
-        userAgent: `${APP_NAME}/${API_VERSION}`
-    },
+  userAgent: `${APP_NAME}/${API_VERSION}`,
 
-    cache: {
-        environment: ENVIRONMENT,
-        // available: redis, memory
-        // "memory" available only in development
-        driver: process.env.CACHE_DRIVER || 'memory',
-        // TTL in seconds
-        ttl: process.env.CACHE_DEFAULT_TTL || DEFAULT_CACHE_TTL
-    },
+  cache: {
+    environment: ENVIRONMENT,
+    // available: redis, memory
+    // "memory" available only in development
+    driver: CACHE_DRIVER,
+    // TTL in seconds
+    ttl: CACHE_DEFAULT_TTL,
+  },
 
-    logger: {
-        name: APP_NAME,
-        level: process.env.DEBUG_LEVEL || 'info'
-    }
+  logger: {
+    // prettyPrint: process.env.NODE_ENV !== 'production',
+    base: { hostname: os.hostname() },
+  },
 };
-const { 'default': targetConfig } = require(`./${ENVIRONMENT}`);
+const { default: targetConfig } = require(`./${ENVIRONMENT}`); // eslint-disable-line import/no-dynamic-require
 const configurations = merge(defaultConfig, targetConfig);
 
 const find = (object, property) => {
-    const elements = Array.isArray(property) ? property : property.split('.');
-    const name = elements[0];
-    const value = object[name];
+  const elements = Array.isArray(property) ? property : property.split('.');
+  const name = elements[0];
+  const value = object[name];
 
-    if (elements.length <= 1) {
-        return value;
-    }
+  if (elements.length <= 1) {
+    return value;
+  }
 
-    if (value === null || typeof value !== 'object') {
-        return undefined;
-    }
+  if (value === null || typeof value !== 'object') {
+    return undefined;
+  }
 
-    return find(value, elements.slice(1));
+  return find(value, elements.slice(1));
 };
 
 export default class Config {
-    /**
+  /**
      * Briefly inspired by https://github.com/lorenwest/node-config
      */
-    static get(property) {
-        const value = find(configurations, property);
+  static get(property) {
+    const value = find(configurations, property);
 
-        if (value === undefined) {
-            throw new Error(`Configuration property ${property} was not found!`);
-        }
-
-        return value;
+    if (value === undefined) {
+      throw new Error(`Configuration property ${property} was not found!`);
     }
+
+    return value;
+  }
 }
