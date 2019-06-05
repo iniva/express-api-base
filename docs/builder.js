1ad06b5948f3e6491d34a6cd4fe17682d843cdb8
Tony <tonyblast@gmail.com>
2019-06-05 20:42:29 -0300

    [master] refactor(docs/builder): Refactored builder

--- docs/builder.js
+++ docs/builder.js
@@ -1,7 +1,5 @@
-'use strict';
-
 const path = require('path');
-const compiler = require('refs-compiler');
+const { default: compiler } = require('refs-compiler');
 const debug = require('debug');
 
 const logger = debug('docs:builder');
@@ -10,21 +8,20 @@ const outputFile = path.resolve(`${__dirname}/openapi.yaml`);
 
 logger('Building documentation');
 try {
-    compiler(inputTemplate, outputFile)
-        .then(results => {
-            logger(`Documentation created in ${results.outputFile}`);
-        })
-        .catch(error => {
-            logger(`An error occurred while building the documentation: ${error.message}`);
-            logger(error.stack);
-        });
-}
-catch (error) {
-    logger(`An error occurred while building the documentation: ${error.message}`);
-    logger(error.stack);
+  compiler(inputTemplate, outputFile)
+    .then(results => {
+      logger(`Documentation created in ${results.outputFile}`);
+    })
+    .catch(error => {
+      logger(`An error occurred while building the documentation: ${error.message}`);
+      logger(error.stack);
+    });
+} catch (error) {
+  logger(`An error occurred while building the documentation: ${error.message}`);
+  logger(error.stack);
 }
 
 process.on('unhandledRejection', error => {
-    logger(`An Unhandled Rejection happened: ${error.message}`);
-    logger(error.stack);
+  logger(`An Unhandled Rejection happened: ${error.message}`);
+  logger(error.stack);
 });
